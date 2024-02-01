0a79e5273b75de0d2463edd644ba26ff6a55afed
kyungjae-Y <yoonkj2008@gmail.com>
2024-02-01 18:24:36 +0900

    Update BoardDAO.java

--- basicJSP/src/main/java/board/BoardDAO.java
+++ basicJSP/src/main/java/board/BoardDAO.java
@@ -1,48 +1,83 @@
 package board;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.List;
 
 public class BoardDAO {
-	private List<BoardVO> boardList;
+	private ArrayList<Board> list;
+	private int cnt;
 
 	public BoardDAO() {
-		// Initialize the boardList, either in the constructor or through a database connection
-		this.boardList = new ArrayList<>();
+		list = new ArrayList<Board>();
+		cnt = 1;
+		init();
+		System.out.println("[정보 로드 완료]");
 	}
 
-	// Add a new board to the list
-	public void addBoard(BoardVO board) {
-		boardList.add(board);
+	private void init() {
+		Board b1 = new Board(cnt++, "작성자1", "2024-02-01", "제목1", "내용1");
+		Board b2 = new Board(cnt++, "작성자2", "2024-02-01", "제목2", "내용2");
+		Board b3 = new Board(cnt++, "작성자3", "2024-02-01", "제목3", "내용3");
+		list.add(b1);
+		list.add(b2);
+		list.add(b3);
 	}
 
-	// Get all boards from the list
-	public List<BoardVO> getAllBoards() {
-		return new ArrayList<>(boardList);
+	public int getTotalData() {
+		return list.size();
 	}
 
-	// Get a specific board by its number
-	public BoardVO getBoardByNo(int boardNo) {
-		for (BoardVO board : boardList) {
-			if (board.getNo() == boardNo) {
-				return board;
-			}
+	public Board getOneBoard(int idx) {
+		return list.get(idx);
+	}
+
+	public void createDummies(int size) {
+		LocalDate date = LocalDate.now();
+		for (int i = 0; i < size; i++) {
+			Board b = new Board(cnt, "작성자" + cnt, date.plusDays(i).toString(), "제목" + cnt, "내용" + cnt);
+			cnt += 1;
+			list.add(b);
 		}
-		return null; // Return null if board is not found
+		System.out.println("[ 더미 추가 완료 ]");
 	}
 
-	// Update a board in the list
-	public void updateBoard(BoardVO updatedBoard) {
-		for (int i = 0; i < boardList.size(); i++) {
-			if (boardList.get(i).getNo() == updatedBoard.getNo()) {
-				boardList.set(i, updatedBoard);
-				break;
+	public void deleteAllData() {
+		//list = new ArrayList<Board>();
+		list.clear();
+		cnt = 1;
+		System.out.println("[ 전체 데이터 삭제완료 ]");
+	}
+
+	public void addOneBoard(String writer, String subject, String contents) {
+		LocalDate date = LocalDate.now();
+		Board b = new Board(cnt++, writer, date.toString(), subject, contents);
+		list.add(b);
+		System.out.println(b);
+		System.out.println("[ 게시글 1개 추가 완료 ]");
+	}
+
+	public void updateOneBoard(String index, String subject, String contents) {
+		int idx = Integer.parseInt(index);
+		Board b = getOneBoard(idx);
+		b.setSubject(subject);
+		b.setContents(contents);
+		System.out.println(b);
+		System.out.println("[ 게시글 1개 수정 완료 ]");
+	}
+
+	public boolean deleteOneBoard(String index) {
+		int idx = -1;
+		try {
+			idx = Integer.parseInt(index);
+			if (idx < 0 || idx >= list.size()) {
+				throw new Exception();
 			}
+			list.remove(idx);
+			System.out.println("[ 게시글 1개 삭제 완료 ]");
+			return true;
+		} catch (Exception e) {
+			System.out.println("[ 게시글 1개 삭제 실패 ]");
+			return false;
 		}
-	}
-
-	// Delete a board from the list
-	public void deleteBoard(int boardNo) {
-		boardList.removeIf(board -> board.getNo() == boardNo);
 	}
 }
